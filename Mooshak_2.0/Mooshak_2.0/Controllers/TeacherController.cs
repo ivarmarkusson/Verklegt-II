c83bf568aed776e02b207013378bc460b47cfd2d
ludvikkemp <ludvik11@ru.is>
2016-05-01 17:16:33 +0000

    Teacher layout

---
+++ Mooshak_2.0/Mooshak_2.0/Controllers/TeacherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooshak_2._0.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult CreateAssignment()
+		{
+			return View();
+		}
+
+		public ActionResult ReviewAssignment()
+		{
+			return View();
+		}
+
+		public ActionResult Assignments()
+		{
+			return View();
+		}
+
+		public ActionResult Settings()
+		{
+			return View();
+		}
+	}
+}
