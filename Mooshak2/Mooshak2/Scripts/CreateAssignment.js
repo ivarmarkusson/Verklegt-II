b66e7e967d2f384bb3a28035c2d830a220c4511c
Búi Eymundsson <Bi Eymundsson>
2016-05-12 17:03:22 +0000

    javascript f createmilestone

--- Mooshak2/Mooshak2/Scripts/CreateAssignment.js
+++ Mooshak2/Mooshak2/Scripts/CreateAssignment.js
@@ -1,22 +1,28 @@
 ﻿$(document).ready(function () {
 
-    var subassignment = $(".subassignment");
-    var addsubassignmentbutton = $(".addsubassignment");
-    var subassnumber = 2;
+    var milestone = $(".milestone");
+    var addmilestonebutton = $(".addmilestone");
+    var milestonenumber = 2;
 
-    addsubassignmentbutton.on("click", function () {
-        subassignment.append($("<div class='subassignment'>"
-            + "    <h3>Sub-Assignment " + subassnumber + "</h3>"
+    addmilestonebutton.on("click", function () {
+        $(this).before($("<div class='milestone'>"
+            + "    <h3>Milestone " + milestonenumber + "</h3>"
             + "    <div class='form-group'>"
             + "        <label>"
-            + "            Sub-Assignment " + subassnumber + " title"
-            + "            <input type='text' class='form-control' id='MilestoneTitle" + subassnumber + "' name='MilestoneTitle" + subassnumber + "'>"
+            + "            Milestone " + milestonenumber + " title"
+            + "            <input type='text' class='form-control' name='Title'>"
             + "        </label>"
             + "    </div>"
+            + "	   <div class='form-group'>"
+			+ "	       <label>"
+			+ "		       Percentage"
+			+ "		       <input type='text' class='form-control' name='Percentage'>"
+			+ "        </label>"
+			+ "    </div>"
             + "    <div class='form-group'>"
             + "        <label>"
             + "            Submission limit"
-            + "            <select name='SubmissionLimit" + subassnumber + "' class='form-control' id='SubmissionLimit" + subassnumber + "'>"
+            + "            <select name='SubmissionLimit' class='form-control'>"
             + "                <option value='1'>1</option>"
             + "                <option value='2'>2</option>"
             + "                <option value='3'>3</option>"
@@ -33,32 +39,31 @@
             + "    <div class='form-group input-pairs'>"
             + "        <label>"
             + "            Input 1"
-            + "            <input type='text' class='form-control' id='MileStone" + subassnumber + "_Input1' name='MileStone" + subassnumber + "_Input1'>"
+            + "            <input type='text' class='form-control' name='MileStone_Input1'>"
             + "        </label>"
             + "        <label>"
             + "            Output 1"
-            + "            <input type='text' class='form-control' id='MileStone" + subassnumber + "_Output1' name='MileStone" + subassnumber + "_Output1'>"
+            + "            <input type='text' class='form-control' name='MileStone_Output1'>"
             + "        </label>"
             + "    </div>"
             + "    <input type='button' value='+ Add input/output' class='btn btn-info addinpoutp'>"
-            + "</div>"
-            + "<br />"));
-        subassnumber++;
+            + "</div>"));
+        milestonenumber++;
     });
 
     var inputpairs = $(".input-pairs");
-    var addpairbutton = $(".addinpoutp");
+    var addpairbutton = $(".addpair");
     var pairnumber = 2;
 
-    $(".addinpoutp").on("click", function () {
-        $(this).prev().append($("<div class='form-group input-pairs'>"
+    addpairbutton.on("click", function () {
+        $(this).before($("<div class='form-group input-pairs'>"
             + "    <label>"
             + "        Input " + pairnumber
-            + "        <input type='text' class='form-control' id='MileStone" + subassnumber + "_Input" + pairnumber + "' name='MileStone" + subassnumber + "_Input" + pairnumber + "'>"
+            + "        <input type='text' class='form-control' name='MileStone_Input" + pairnumber + "'>"
             + "    </label>"
             + "    <label>"
             + "        Output " + pairnumber
-            + "        <input type='text' class='form-control' id='MileStone" + subassnumber + "_Output" + pairnumber + "' name='MileStone" + subassnumber + "_Output" + pairnumber + "'>"
+            + "        <input type='text' class='form-control' name='MileStone_Output" + pairnumber + "'>"
             + "    </label>"
             + "</div>"));
         pairnumber++;
