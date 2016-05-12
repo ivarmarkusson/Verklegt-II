8a4d3c7660e46df6bbcefa80f8d4d750846aef4f
Búi Eymundsson <Bi Eymundsson>
2016-05-12 20:50:33 +0000

    Studentviewsofl

--- Mooshak2/Mooshak2/Controllers/StudentController.cs
+++ Mooshak2/Mooshak2/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mooshak2.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +10,21 @@ namespace Mooshak2.Controllers
     public class StudentController : Controller
     {
         // GET: Student
+        [Authorize(Roles = "Student")]
         public ActionResult SubmitSolution()
         {
             return View();
         }
 
-        public ActionResult Description()
+        [Authorize(Roles = "Student")]
+        [HttpPost]
+        public ActionResult SubmitSolution(SubmissionViewModel model)
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Student")]
+        public ActionResult YourSubmissions()
         {
             return View();
         }
