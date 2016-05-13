1592a1f61c79aac19d2fbdba9a49a69883114b3a
ivarmarkusson <ivarmar15@ru.is>
2016-05-13 00:23:55 +0000

    Submit solution - Student

--- Mooshak2/Mooshak2/Controllers/StudentController.cs
+++ Mooshak2/Mooshak2/Controllers/StudentController.cs
@@ -1,6 +1,9 @@
-﻿using Mooshak2.Models.ViewModels;
+﻿using Mooshak2.Models;
+using Mooshak2.Models.Entities;
+using Mooshak2.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,19 +12,59 @@ namespace Mooshak2.Controllers
 {
     public class StudentController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
         // GET: Student
         [Authorize(Roles = "Student")]
         public ActionResult SubmitSolution()
         {
-            return View();
+            var milestones = _db.Milestones.ToList();
+            
+            List<MilestoneViewModel> models = new List<MilestoneViewModel>();
+            
+            foreach(var milestone in milestones)
+            {
+                MilestoneViewModel newModel = new MilestoneViewModel();
+
+                newModel.ID = milestone.ID;
+                newModel.Percentage = milestone.Percentage;
+                newModel.Title = milestone.Title;
+
+                models.Add(newModel);
+            }
+
+            return View(models);
         }
 
+        // POST: Student
         [Authorize(Roles = "Student")]
         [HttpPost]
         public ActionResult SubmitSolution(SubmissionViewModel model)
         {
-            return View();
+            Submission newSubmission = new Submission();
+
+            newSubmission.MilestoneID = model.MilestoneID;
+
+            HttpPostedFileBase file = model.SubmissionFile;
+
+            if(file.ContentLength > 0 && file != null)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/Submissions"), filename);
+
+                newSubmission.SubmissionPath = path;
+                newSubmission.SubmissionFileName = filename;
+                file.SaveAs(path);
+            }
+
+
+            _db.Submissions.Add(newSubmission);
+            _db.SaveChanges();
+        
+
+            return Redirect("~/Student/YourSubmissions");
         }
+
 
         [Authorize(Roles = "Student")]
         public ActionResult YourSubmissions()
