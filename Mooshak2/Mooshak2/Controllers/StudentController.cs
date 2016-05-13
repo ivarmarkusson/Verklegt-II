ed7c3a4e5232e445627c5460837c307a103c5ed7
Búi Eymundsson <Bi Eymundsson>
2016-05-13 01:28:31 +0000

    compiler

--- Mooshak2/Mooshak2/Controllers/StudentController.cs
+++ Mooshak2/Mooshak2/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using Mooshak2.Models.Entities;
 using Mooshak2.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -46,21 +47,40 @@ namespace Mooshak2.Controllers
             newSubmission.MilestoneID = model.MilestoneID;
 
             HttpPostedFileBase file = model.SubmissionFile;
+            var filename = Path.GetFileName(file.FileName);
+            var path = Path.Combine(Server.MapPath("~/Content/Submissions"), filename);
 
-            if(file.ContentLength > 0 && file != null)
+            if (file.ContentLength > 0 && file != null)
             {
-                var filename = Path.GetFileName(file.FileName);
-                var path = Path.Combine(Server.MapPath("~/Content/Submissions"), filename);
-
                 newSubmission.SubmissionPath = path;
                 newSubmission.SubmissionFileName = filename;
                 file.SaveAs(path);
             }
 
-
             _db.Submissions.Add(newSubmission);
             _db.SaveChanges();
-        
+
+            var workingFolder = Server.MapPath("~/Content/Submissions");
+            var exeFileName = filename.Replace(".cpp", ".exe");
+            var exeFilePath = workingFolder + exeFileName;
+            var compilerFolder = "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\";
+
+            Process compiler = new Process();
+            compiler.StartInfo.FileName = "cmd.exe";
+            compiler.StartInfo.WorkingDirectory = workingFolder;
+            compiler.StartInfo.RedirectStandardInput = true;
+            compiler.StartInfo.RedirectStandardOutput = true;
+            compiler.StartInfo.UseShellExecute = false;
+
+            compiler.Start();
+            compiler.StandardInput.WriteLine("\"" + compilerFolder + "vcvars32.bat" + "\"");
+            compiler.StandardInput.WriteLine("cl.exe /nologo /EHsc " + filename);
+            compiler.StandardInput.WriteLine("exit");
+            string output = compiler.StandardOutput.ReadToEnd();
+            compiler.WaitForExit();
+            compiler.Close();
+
+
 
             return Redirect("~/Student/YourSubmissions");
         }
